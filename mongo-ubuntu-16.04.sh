d4a5fa9f62c4158ab7c77979347f128c7ae0bcc5
administrator <administrator@assw12.ing.puc.cl>
2016-10-13 10:57:14 -0300

    finish mongo script

--- mongo-ubuntu-16.04.sh
+++ mongo-ubuntu-16.04.sh
@@ -2,8 +2,6 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
-sudo mkdir -p etc/systemd/system
-sudo touch etc/systemd/system/mongodb.service
 sudo cat mongo-service.txt | sudo tee /etc/systemd/system/mongodb.service
 sudo systemctl start mongodb
 sudo systemctl status mongodb
